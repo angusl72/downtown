ded3d959defae7ae636876267344b5caf2567263
Shawn McSparron <shawnmc_28@hotmail.com>
2022-12-17 14:28:19 +1100

    added pundit policies

---
+++ app/policies/image_policy.rb
@@ -0,0 +1,33 @@
+class ImagePolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+
+    def show?
+      true
+    end
+
+    def new?
+      true
+    end
+
+    def create?
+      new?
+    end
+
+    def destroy?
+      record.user == user
+    end
+
+    def edit?
+      record.user == user
+    end
+
+    def update?
+      edit?
+    end
+
+  end
+end
