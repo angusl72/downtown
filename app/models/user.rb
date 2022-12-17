7c441395815f2885c01b7632a6f7f7897b6ba644
Shawn McSparron <shawnmc_28@hotmail.com>
2022-12-17 16:18:26 +1100

    adding seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :images, dependent: :destroy
   has_many :comments, dependent: :destroy
+  validates :username, presence: true
 
   # data validations - TO DO
 end
