e53f5ca10be3f01d61aff2d518fe96a0bb9e2d7f
Shawn McSparron <shawnmc_28@hotmail.com>
2023-01-07 11:24:05 +1100

    created stimulus controller

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SearchboxController from "./searchbox_controller"
+application.register("searchbox", SearchboxController)
