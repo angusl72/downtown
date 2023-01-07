3b097c0d8460ba42be19d29e1816a5d4de78bc1d
Shawn McSparron <shawnmc_28@hotmail.com>
2023-01-07 14:26:16 +1100

    changed stimulus

--- app/javascript/controllers/searchbox_controller.js
+++ app/javascript/controllers/searchbox_controller.js
@@ -2,7 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="searchbox"
 export default class extends Controller {
+  static targets = [ "searchbarNav", "searchbarDropdown"]
+
   connect() {
+    console.log("test comment")
+  }
+
+  searchbarDropdown(){
+    console.log(this)
+    this.searchbarDropdownTarget.hidden = false
     console.log("test comment")
   }
 }
