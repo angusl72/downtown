eba7b05334202bfa51032fba326c792c6a41d87a
Shawn McSparron <shawnmc_28@hotmail.com>
2023-01-25 15:28:47 +1100

    new seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,80 +1,4 @@
-# # This file should contain all the record creation needed to seed the database with its default values.
-# # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-# #
-
-# puts "Cleaning users.."
-# User.destroy_all
-# puts "Cleaning Images.."
-# Image.destroy_all
-# Comment.destroy_all
-# puts ""
-# puts "Creating Test account.."
-
-# # Create test user
-# User.create!(
-#   email: "test@test.com",
-#   password: "123456",
-#   username: "test",
-#   city: "Sydney, Australia"
-# )
-# puts "Test User Created"
-
-# # Creating 10 users
-# 4.times do
-#   user = User.create!(
-#     username: Faker::Internet.username,
-#     email: Faker::Internet.email,
-#     password: "123456",
-#     city: "Sydney, Australia"
-#   )
-#   puts "Creating User: #{user.username}"
-
-#   # Creating 2 images per user
-#   2.times do
-#     image = Image.create!(
-#       user_id: user.id,
-#       address: ["161 Richardson St", "13 rickard ave", "20 canning st", "103 canning st", "20 collins st"].sample,
-#       options: Array.new([%w[trees bicycle cafe green mural].sample]),
-#       image_saved: true,
-#       before_photo_base_url: [
-#         "https://maps.googleapis.com/maps/api/streetview?size=640x512&location=161%20Richardson%20St&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}",
-#         "https://maps.googleapis.com/maps/api/streetview?size=640x512&location=13%20rickard%20ave&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}",
-#         "https://maps.googleapis.com/maps/api/streetview?size=640x512&location=103%20canning%20st&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}",
-#         "https://maps.googleapis.com/maps/api/streetview?size=640x512&heading=1&location=103%20canning%20st&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}",
-#         "https://maps.googleapis.com/maps/api/streetview?size=640x512&heading=90&location=161%20collins%20st%20melbourne&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}"
-#       ].sample
-#     )
-
-#     puts "Img Adddress: #{image.address}"
-
-#     puts "Before photo attached:  #{image.before_photo.attached?}"
-
-#     # After photo
-#     image.after_photo.attach(io: File.open(Rails.root.join("app/assets/images/#{rand(1..5)}-2.jpg")), filename: "#{image.id}-after.jpg")
-#     image.save!
-#     puts "After photo attached: #{image.after_photo.attached?}"
-
-#     # Creating 2 comments per photo
-#     2.times do
-#       Comment.create!(
-#         user_id: User.all.sample.id,
-#         image_id: image.id,
-#         text: Faker::Restaurant.review
-#       )
-#     end
-#   end
-#   puts "------------------------------------------"
-# end
-
-# puts "-----------------"
-# puts "---------FINISHED SEEDS!---------"
-# puts "-----------------"
-
-
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
+# NOTE need to disable after_create :attach_before_photo in the model before running seed
 
 puts "Cleaning users.."
 User.destroy_all
@@ -116,48 +40,102 @@ User.create!(
 )
 puts "User Ahmet Created"
 
+def create_user_image_comments(object)
+  puts "---------------------------"
+  user = User.create!(
+    email: object[:email],
+    password: object[:password],
+    username: object[:username],
+    city: object[:city]
+  )
 
+  puts "User #{user.username} Created"
 
-user = User.create!(
+  image = Image.create!(
+    user_id: user.id,
+    address: object[:full_address],
+    options: object[:options],
+    image_saved: true,
+    before_photo_base_url: "https://maps.googleapis.com/maps/api/streetview?size=640x512&location=#{object[:full_address]}&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}"
+  )
+
+  puts "Img Adddress: #{image.address}"
+  image.before_photo.attach(io: File.open(Rails.root.join("app/assets/images/seed/#{object[:image_name]}.jpeg")), filename: "before_photo_#{image.id}.jpg")
+  puts "Before photo attached:  #{image.before_photo.attached?}"
+  # After photo
+  image.after_photo.attach(io: File.open(Rails.root.join("app/assets/images/seed/#{object[:image_name]} - AI.jpeg")), filename: "#{image.id}-after.jpg")
+  image.save!
+  puts "After photo attached: #{image.after_photo.attached?}"
+
+  puts "-----Creating comments-----"
+  comment = Comment.create!(
+    user_id: User.all.excluding(User.last).sample.id,
+    image_id: image.id,
+    text: object[:comment_one]
+  )
+  puts "#{comment.user.username} - #{comment.text}"
+
+  comment = Comment.create!(
+    user_id: User.all.excluding(User.last).sample.id,
+    image_id: image.id,
+    text: object[:comment_two]
+  )
+  puts "#{comment.user.username} - #{comment.text}"
+  puts "-----Finished comments-----"
+end
+
+object = {
   email: "amelia92@outlook.com",
-  password: "Amelia921234",
+  password: "amelia921234",
   username: "Amelia92",
-  city: "Sydney, Australia"
-)
-puts "User Amelia92 Created"
+  city: "Sydney, Australia",
 
-image = Image.create!(
-  user_id: user.id,
-  address: "8 Elizabeth St, Sydney, New South Wales",
-  options: %w[cafe],
-  image_saved: true,
-  before_photo_base_url: "https://maps.googleapis.com/maps/api/streetview?size=640x512&location=8 Elizabeth St Sydney New South Wales&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}"
-)
+  full_address: "8 Elizabeth St, Sydney, New South Wales",
+  options: %w[restaurant],
+  image_name: "8 Elizabeth St",
 
-photo = "8 Elizabeth St - AI.jpeg"
+  comment_one: "Would love to eat there!",
+  comment_two: "Looks so nice"
+}
+create_user_image_comments(object)
 
-puts "Img Adddress: #{image.address}"
+# image = Image.create!(
+#   user_id: user.id,
+#   address: "8 Waldron Rd, Kincumber, New South Wales",
+#   options: %w[playground],
+#   image_saved: true,
+#   before_photo_base_url: "https://maps.googleapis.com/maps/api/streetview?size=640x512&location=8 Waldron Rd Kincumber New South Wales&key=#{ENV['GOOGLE_STREET_VIEW_API_KEY']}"
+# )
+# photo = "8 Waldron Rd - AI.jpeg"
+# puts "Img Adddress: #{image.address}"
+# puts "Before photo attached:  #{image.before_photo.attached?}"
+# # After photo
+# image.after_photo.attach(io: File.open(Rails.root.join("app/assets/images/seed/#{photo}")), filename: "#{image.id}-after.jpg")
+# image.save!
+# puts "After photo attached: #{image.after_photo.attached?}"
 
-puts "Before photo attached:  #{image.before_photo.attached?}"
+# puts "-----Creating comments-----"
+# Comment.create!(
+#   user_id: User.all.sample.id,
+#   image_id: image.id,
+#   text: "Perfect spot for it"
+# )
+# Comment.create!(
+#   user_id: User.all.sample.id,
+#   image_id: image.id,
+#   text: "We need more of those around"
+# )
 
-# After photo
-image.after_photo.attach(io: File.open(Rails.root.join("app/assets/images/seed/#{photo}")), filename: "#{image.id}-after.jpg")
-image.save!
-puts "After photo attached: #{image.after_photo.attached?}"
 
-Comment.create!(
-  user_id: User.all.sample.id,
-  image_id: image.id,
-  text: "Would love to get a coffee there"
-)
-puts "------------------------------------------"
+puts "---------------------------"
 
-Comment.create!(
-  user_id: User.all.sample.id,
-  image_id: image.id,
-  text: "That looks so much better!"
-)
-puts "------------------------------------------"
+
+
+
+
+
+
+
 
 
 puts "-----------------"
