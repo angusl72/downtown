dfc41abd2ff0ee84324174814d51aaae67ce1acd
Shawn McSparron <65693795+smcsparron@users.noreply.github.com>
2023-02-08 23:14:05 +1100

    api working with new params

--- app/models/image.rb
+++ app/models/image.rb
@@ -21,90 +21,49 @@ class Image < ApplicationRecord
   end
 
   def generate_image_variations
-    # use before_photo_base_url to generate variations using api
-    # Api returns json with image information (save this image to after_photo)
-    # debugger
-
     image = MiniMagick::Image.open(before_photo_base_url)
     image.format "PNG"
     # image.resize('640x512') # only needed if other size is returned
 
-    url = URI("https://api.stability.ai/v1alpha/generation/stable-diffusion-512-v2-1/image-to-image")
+    engine = 'stable-diffusion-512-v2-1'
+    form_data = [
+      ['init_image', image.to_blob], # Street view image in base64 format
+      ['samples', '1'], # How many images to generate
+      ['width', '640'],
+      ['height', '512'],
+      ['init_image_mode', "IMAGE_STRENGTH"],
+      ['image_strength', '0.7'], # [0..1] How much influence the init_image has on the diffusion process. Values close to 1 will yield images very similar to the init_image while values close to 0 will yield images wildly different than the init_image.
+      ['cfg_scale', '17'], # [0..35] How strictly the diffusion process adheres to the prompt text (higher values keep your image closer to your prompt
+      ['sampler', 'K_DPM_2_ANCESTRAL'],
+      ['clip_guidance_preset', 'FAST_GREEN'],
+      ['steps', '100'], # [10..150] Number of diffusion steps to run
+      ['text_prompts[0][text]:', 'A photo of beautiful architectural street with great urban design '],
+      ['text_prompts[0][weight]:', '0.5'], # [0..1] How much weighting the prompt will have on the image
+      ['text_prompts[3][text]:', 'disfigured, impressionistic'],
+      ['text_prompts[3][weight]:', '-1']
+    ]
+
+    unless self.options.empty? # This will remove the option radio checkboxes from form data if none are selected. Blanks cause API error
+      form_data.append(['text_prompts[1][text]:', self.options.join(', ')])
+      form_data.append(['text_prompts[1][weight]:', '1'])
+    end
+
+    unless self.custom_option.empty? # This will remove the custom options from form data if left blank. Blanks cause API error
+      form_data.append(['text_prompts[2][text]:', self.custom_option])
+      form_data.append(['text_prompts[2][weight]:', '1'])
+    end
+
+    url = URI("https://api.stability.ai/v1beta/generation/#{engine}/image-to-image")
     https = Net::HTTP.new(url.host, url.port)
     https.use_ssl = true
     request = Net::HTTP::Post.new(url)
     request["Authorization"] = ENV.fetch("STABILITY_AI_KEY")
     request["Accept"] = "application/json"
 
-    options = {
-      cfg_scale: 15,
-      clip_guidance_preset: "FAST_BLUE",
-      height: 512,
-      sampler: "K_DPMPP_2S_ANCESTRAL",
-      samples: 1,
-      seed: 0,
-      step_schedule_end: 0.01,
-      step_schedule_start: 0.4,
-      steps: 102,
-      text_prompts: [
-        {
-          text: "A photo of beautiful architectural street with great urban design ",
-          weight: 0.5
-        },
-        {
-          text: self.options.join(', '),
-          weight: 1
-        },
-        {
-          text: self.custom_option,
-          weight: 1
-        }
-        # {
-        #   text: "disfigured, ugly, , boring, oversaturated, low-res, blurry, blurry, blur",
-        #   weight: -1
-        # }
-      ],
-      width: 512
-    }
-
-    form_data = [['init_image', image.to_blob], ['options', options.to_json]]
     request.set_form form_data, 'multipart/form-data'
     response = https.request(request)
     response.read_body
     parsed_response = JSON.parse(response.read_body)
     parsed_response["artifacts"][0]["base64"]
   end
-
-  # Text generation method not currently in use
-  # def generate_text_variations
-  #   # use before_photo to generate variations using api
-  #   # Api returns json with image information (save this image to after_photo)
-  #   # debugger
-  #   headers = {
-  #     Authorization: "sk-5NLUcaVC3X6jSekE3PohI4MNklrxxsLUKImERlSt5V5SBCJg",
-  #     Accept: "application/json",
-  #     "Content-Type": "application/json"
-  #   }
-
-  #   body = {
-  #     cfg_scale: 7,
-  #     clip_guidance_preset: "FAST_BLUE",
-  #     height: 512,
-  #     sampler: "K_DPM_2_ANCESTRAL",
-  #     samples: 1,
-  #     seed: 0,
-  #     steps: 20,
-  #     text_prompts: [
-  #       {
-  #         text: "A lighthouse and many trees on a cliff",
-  #         weight: 1
-  #       }
-  #     ],
-  #     width: 512
-  #   }
-  #   response = HTTParty.post("https://api.stability.ai/v1alpha/generation/stable-diffusion-512-v2-1/text-to-image", body: body.to_json, headers: headers)
-  #   base_64_result = response["artifacts"][0]["base64"]
-  # end
-
-
 end
